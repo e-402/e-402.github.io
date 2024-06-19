ac4fa64f96adc6514138ef3d85182c14fddcee2f
e402 <91824835+e-402@users.noreply.github.com>
2024-06-19 13:46:12 +0000

    Downloaded Strapi

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly CONTENTFUL_SPACE_ID: string;
   readonly CONTENTFUL_DELIVERY_TOKEN: string;
   readonly CONTENTFUL_PREVIEW_TOKEN: string;
+  readonly STRAPI_URL: string;
 }
 
 declare module "*.riv" {
