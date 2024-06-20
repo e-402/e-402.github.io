d7a33486b17ee35ea685908a8ba61246507de9a4
e402 <91824835+e-402@users.noreply.github.com>
2024-06-20 11:42:00 +0000

    Defined collection schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,21 @@
-import { defineCollection } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
 import { rssSchema } from '@astrojs/rss';
 
 const blog = defineCollection({
   schema: rssSchema,
 });
 
-export const collections = { blog };
+const blogCollection = defineCollection({
+  type: 'content', // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  'blog': blogCollection,
+};
+
+
